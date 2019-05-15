5c3b17e2be5d9400c65cf1a4fe4cb95271cf560f
Hasan Anwar <hasan_anwar@ucp.edu.pk>
2019-05-15 09:41:27 +0500

    Merge remote-tracking branch 'remotes/master/master'
    # Conflicts:
    #	pro/admin/insert_product.html

--- lec 13/script.js
+++ lec 13/script.js
@@ -1,29 +1,40 @@
 
-var database={
+var database=[{
     user:"HASAN",
-    password:123
+    password:"123"
 
-}
+},
+    {
+        user: "jamal",
+        password: "987"
+    },
+    {
+        user:"ali",
+        password:"567"
+    }
+    ]
 var newsfeed=[{
     user:"ali",
     content:"I am happy today"
 },
     {
-        user: "umer",
+        user: "HASAN",
         content: "I am ha ha ha"
     }
 
 ]
-var j=function () {
-    a=prompt("Enter name");
-    b=prompt("Enter password");
-    if(a===database.user && b===database.password)
-    {
-        console.log(newsfeed);
-    }else
-    {
-        alert("wrong password");
-    }
+a=prompt("Enter name");
+b=prompt("Enter password");
 
+        for(var i=0;i<database.length;i++) {
+            if (a === database[i].user && b === database[i].password) {
+                for (var j=0;j<newsfeed.length;j++) {
+                    if (a === newsfeed[j].user) {
+                        console.log(newsfeed[j]);
 
-}
+                    }
+                }
+            }
+
+        }
+
